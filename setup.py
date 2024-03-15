88f480f0aaa32fc52af7720bff4d5aa6cc297963
unknown <kishang177@gmail.com>
2024-03-15 21:36:32 +0530

    setup for install all dependency like pandas and numpy in an easy way part 2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
 
 
 PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "kishan"
 DESCRIPTION = "A simple housing price predictor using machine learning."
 PACKAGES=["housing"]
@@ -13,13 +13,13 @@ REQUIREMENT_FILE_NAME = "requirement.txt"
 
 def get_requirements_list()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirements_file:
-        return requirements_file.readlines()
+        return requirements_file.readlines().remove("-e .")
 
 setup( 
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
